d13487028230f15b7c5161fceef202959c4bf245
EC2 Default User <m3n7m3.97.415@gmail.com>
2022-01-06 01:26:55 +0000

    デザイン完了

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,16 @@
 class LikesController < ApplicationController
   
-  def create
+ def create
     food_image = FoodImage.find(params[:food_image_id])
     like = current_user.likes.new(food_image_id: food_image.id)
     like.save
-    redirect_to food_image_path(food_image)  end
+    redirect_to photo_path(food_image)  
+ end
 
   def destroy
     food_image = FoodImage.find(params[:food_image_id])
     like = current_user.likes.find_by(food_image_id: food_image.id)
     like.destroy
-    redirect_to food_image_path(food_image)  
+    redirect_to photo_path(food_image)  
   end
 end
